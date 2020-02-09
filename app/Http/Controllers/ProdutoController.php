1082b5fe1daa0c03f0f3b22afe9839504da8a147
adeonita <adeonita.sousa@gmail.com>
2020-02-09 15:41:48 -0300

    Implementaçao do metodo mostra

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -11,18 +11,15 @@
          ***/
         public function lista(){
             
-            $html = '<h1> Listagem de produtos com Laravel</h1>';
-            $html = $html . '<ul>';  //Crio uma lista não ordenada por concatenação
-            //Chamo o método estatico( select() ) da classe DB e passo a query. Atribuo tudo isso a $produtos
-            //$variavel = Classe::métodoEstatico('query)
             $produtos = DB::select('select * from produtos'); 
 
-            foreach ($produtos as $p) {  //Percorro o array produtos atribuindo cada ítem individual a p
-                $html .=  '<li>Nome: '. $p->nome . ' - Descrição: '. $p->descricao . '</li><br>';  //Acesso os atributos nome e descrição de cada produto. Concatenando da variavel html
-            }
-            $html = $html . '</ul>'; //Fecho a lista 
-            
-            return $html;  //A lista já é uma string, então não preciso do print, basta retorna-la
+            return view('lista', ['produtos' => $produtos]);
+        }
+
+        public function mostra(){
+            $produtos = DB::select('select * from produtos');
+
+            return view('detalhes', ['produtos' => $produtos]);
         }
     }
 
