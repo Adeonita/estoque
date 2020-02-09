ef603f4bd4468725798219713a3e95c21fae64e8
adeonita <adeonita.sousa@gmail.com>
2020-02-09 15:43:17 -0300

    Modificaçao do texto da home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Estoque 
                 </div>
 
                 <div class="links">
