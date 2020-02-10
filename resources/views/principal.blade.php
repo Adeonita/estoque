98465974577901981573af781ca35ee53bcbcf8c
adeonita <adeonita.sousa@gmail.com>
2020-02-09 21:12:32 -0300

    Criaçao de template comun as views

---
+++ resources/views/principal.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="/css/app.css">
+        <title>Controle de Estoque</title>
+    </head>
+    <body>
+        @yield('conteudo');
+    </body>
+</html>
