fa9d0f0d797290d24987590f893ac98b6ebb75a5
adeonita <adeonita.sousa@gmail.com>
2020-02-09 21:42:55 -0300

    Demarcaçao da sessao

--- resources/views/detalhes.blade.php
+++ resources/views/detalhes.blade.php
@@ -1,10 +1,13 @@
 @extends('principal')
 
-<h1>Detalhes do produto <?= $produto->nome?></h1>
+@section('conteudo')
+    <h1>Detalhes do produto <?= $produto->nome?></h1>
 
-<ul>
-    <li><b>Nome: </b><?print $produto->nome; ?></li>
-    <li><b>Descrição: </b><?print $produto->descricao; ?></li>
-    <li><b>Quantidade: </b><?print $produto->quantidade; ?></li>
-    <li><b>Valor: </b><?print $produto->valor; ?></li>
-</ul>
+    <ul>
+        <li><b>Nome: </b><?print $produto->nome; ?></li>
+        <li><b>Descrição: </b><?print $produto->descricao; ?></li>
+        <li><b>Quantidade: </b><?print $produto->quantidade; ?></li>
+        <li><b>Valor: </b><?print $produto->valor; ?></li>
+    </ul>
+@stop
+
