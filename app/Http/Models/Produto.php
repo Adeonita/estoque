46caa0303582299c0ce547491498525b42067029
adeonita <adeonita.sousa@gmail.com>
2020-02-13 15:36:14 -0300

    Utilizaçao de fields e guarded

--- app/Http/Models/Produto.php
+++ app/Http/Models/Produto.php
@@ -4,6 +4,12 @@
     use Illuminate\Database\Eloquent\Model;
 
     class Produto extends Model{
+        
         protected $table = 'produtos';
+        public $timestamps = false;
+        protected $fillable = array('nome', 'descricao', 'quantidade', 'valor');  /**Especificação de quais parâmetros devem vir
+                                                                                    * no corpo da requisição
+                                                                                    **/
+        protected $guarded = ['id']; /**Especificação de quais parametros não devem ser inseridos na requisição*/
     }
 ?>
