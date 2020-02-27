bcaddd67955e810890cddc777b71f357b92feeab
adeonita <adeonita.sousa@gmail.com>
2020-02-26 23:41:54 -0300

    Implementaçao da interface JWT no model user

--- app/Http/Models/User.php
+++ app/Http/Models/User.php
@@ -2,12 +2,28 @@
 
 namespace estoque\Http\Models;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
     /**
      * The attributes that are mass assignable.
